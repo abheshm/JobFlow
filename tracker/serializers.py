8f809062175a2b26095054224e3145ce3ab9dc32
Abhesh <abheshm@gmail.com>
2026-05-13 10:57:29 +0530

    Authentication initialized

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
 from .models import JobApplication
+from django.contrib.auth.models import User
 
 class JobApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobApplication
         fields = '__all__'
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = User
+
+        fields = ['username', 'password']
+
+        extra_kwargs = {
+
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+
+        user = User.objects.create_user(
+
+            username=validated_data['username'],
+
+            password=validated_data['password']
+        )
+
+        return user
