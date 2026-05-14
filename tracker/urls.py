8d21413e9cbd414b4a6a2e04f7f6dbf377c22635
Abhesh <abheshm@gmail.com>
2026-05-14 11:47:30 +0530

    authentication initialized

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns=[
     path('jobs/',views.JobApplicationList.as_view(),name='job-list'),
     path('jobs/<int:pk>/',views.JobApplicationDetail.as_view(),name='job-detail'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('register-page/', views.register_page, name='register-page'),
+    path('login/', views.LoginView.as_view(), name='login'),
 
     path('',views.job_list_view, name='job_list'),
     path('delete_job/<int:pk>/',views.delete_job,name='delete_job'),
--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from .models import JobApplication
-from .serializers import JobApplicationSerializer
+from .serializers import JobApplicationSerializer , RegisterSerializer
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 class JobApplicationList(APIView):
@@ -60,10 +62,48 @@ class JobApplicationDetail(APIView):
         return Response({'message': 'Deleted successfully '})
 
 
+class RegisterView(APIView):
+
+    def post(self, request):
+
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response(
+                {'message': 'User registered successfully'},
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+
+    def post(self, request):
+
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username ,password=password)
+
+        if user is not None:
+            login(request,user)
+
+            return Response({
+                'message': 'Login Successful'
+            })
+        
+        return Response ({
+            'error': 'Invalid credentials'
+        }, status=status.HTTP_400_BAD_REQUEST)
 
 
 
+def register_page(request):
 
+    return render(request, 'register.html')
 
 def job_list_view(request):
     return render(request, 'job_list.html')
